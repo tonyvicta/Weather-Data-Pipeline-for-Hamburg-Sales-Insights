90dddcefcda5803234c17fe91956f653597600ef
Tony Tawakali <consulting-@outlook.com>
2025-05-02 20:55:01 +0100

    Create updated_hamburg_sales.sql

---
+++ 01_transformation /updated_hamburg_sales.sql
@@ -0,0 +1,28 @@
+USE ROLE accountadmin;
+USE WAREHOUSE compute_wh;
+USE DATABASE tasty_bytes;
+
+-- Apply UDFs and confirm successful execution
+CREATE OR REPLACE VIEW harmonized.weather_hamburg -- add name of view
+AS
+SELECT
+    fd.date_valid_std AS date,
+    fd.city_name,
+    fd.country_desc,
+    ZEROIFNULL(SUM(odv.price)) AS daily_sales,
+    ROUND(AVG(fd.avg_temperature_air_2m_f),2) AS avg_temperature_fahrenheit,
+    ROUND(AVG(analytics.fahrenheit_to_celsius(fd.avg_temperature_air_2m_f)),2) AS avg_temperature_celsius,
+    ROUND(AVG(fd.tot_precipitation_in),2) AS avg_precipitation_inches,
+    ROUND(AVG(analytics.inch_to_millimeter(fd.tot_precipitation_in)),2) AS avg_precipitation_millimeters,
+    MAX(fd.max_wind_speed_100m_mph) AS max_wind_speed_100m_mph
+FROM harmonized.daily_weather_v fd
+LEFT JOIN harmonized.orders_v odv
+    ON fd.date_valid_std = DATE(odv.order_ts)
+    AND fd.city_name = odv.primary_city
+    AND fd.country_desc = odv.country
+WHERE 1=1
+    AND fd.country_desc = 'Germany'
+    AND fd.city = 'Hamburg'
+    AND fd.yyyy_mm = '2022-02'
+GROUP BY fd.date_valid_std, fd.city_name, fd.country_desc
+ORDER BY fd.date_valid_std ASC;
